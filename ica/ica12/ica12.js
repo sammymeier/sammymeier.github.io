6e668197a25f559b2d3fa6057c3b9b4084ba0b0b
sammymeier <123104018+sammymeier@users.noreply.github.com>
2023-04-11 10:26:47 -0600

    Update ica12.js

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -18,3 +18,102 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+        ctx.fill();
+    }
+    update()  {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }           
+
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+          
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+          
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+    collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+}
+
+const balls = [];
+
+// how many balls do we want?
+while (balls.length < 25) {
+    const size = random(10,20); // determining hypothetical smallest and largest ball we could have
+    const ball = new Ball(
+        random(0 + size, width - size), // random postion
+        random(0 + size, height - size), // random postion
+        random(-5, 5), // random speed x
+        random(-5, 5), // random speed y
+        randomRGB(), 
+        size
+    );
+
+    balls.push(ball);
+}
+
+function loop() {
+    ctx.fillStyle = "rgba(0,0,0,0.25)";
+    ctx.fillRect(0,0,width,height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+
+
+//const testBall = new Ball(100, 100, 2, 2, "red", 10);
+//const testBall1 = new Ball(400, 238, 2, 2, "blue", 100);
+//const testBall2 = new Ball(700, 438, 2, 2, "aquamarine", 500);
+//testBall.draw();
+
+//let i = 0;
+//while (i<100) {
+    //testBall.update();
+    //i += 1;
+//}
+
+
+//testBall1.draw();
+//testBall2.draw();
