9e63fd547e46514e7d1235a10ce9feee4af0bdc3
sammymeier <123104018+sammymeier@users.noreply.github.com>
2023-05-09 15:41:30 -0600

    Update wa15.js

--- wa/wa15/wa15.js
+++ wa/wa15/wa15.js
@@ -1,41 +1,40 @@
-var diceValues = [];
-var score = 0;
+ // Player name
+ var player1 = "Player 1";
+ var player2 = "Player 2";
 
-function rollDice() {
-	// Clear previous values
-	diceValues = [];
-	score = 0;
+ var diceImages = ["https://media.geeksforgeeks.org/wp-content/uploads/20200508141000/dice1.png", "https://media.geeksforgeeks.org/wp-content/uploads/20200508141001/dice2.png", "https://media.geeksforgeeks.org/wp-content/uploads/20200508141003/dice3.png", "https://media.geeksforgeeks.org/wp-content/uploads/20200508141004/dice4.png", "https://media.geeksforgeeks.org/wp-content/uploads/20200508141005/dice5.png", "https://media.geeksforgeeks.org/wp-content/uploads/20200508141005/dice5.png"];
 
-	// Roll dice and store values
-	for (var i = 0; i < 5; i++) {
-		var value = Math.floor(Math.random() * 6) + 1;
-		diceValues.push(value);
-	}
+ // Function to change player name
+ function editNames() {
+     player1 = prompt("Change Player1 name");
+     player2 = prompt("Change player2 name");
 
-	// Sort dice values
-	diceValues.sort();
+     document.querySelector("p.Player1").innerHTML = player1;
+     document.querySelector("p.Player2").innerHTML = player2;
+ }
 
-	// Calculate score
-	if (diceValues[0] === 1 && diceValues[1] === 2 && diceValues[2] === 3 && diceValues[3] === 4 && diceValues[4] === 5) {
-		// Captain, Ship, Crew
-		score = 1000;
-	} else if (diceValues[0] === diceValues[2] || diceValues[1] === diceValues[3] || diceValues[2] === diceValues[4]) {
-		// Three of a kind
-		score = diceValues[2] * 100;
-	} else if (diceValues[0] === diceValues[3] || diceValues[1] === diceValues[4]) {
-		// Four of a kind
-		score = diceValues[2] * 1000;
-	} else if (diceValues[0] === 1 && diceValues[1] === 1 && diceValues[2] === 1) {
-		// Three ones
-		score = 1000;
-	} else if (diceValues[0] === 6 && diceValues[1] === 6 && diceValues[2] === 6) {
-		// Three sixes
-		score = 600;
-	}
+ // Function to roll dice
+function rollTheDice() {
+    setTimeout(function () {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-	// Display dice values and score
-	var diceValuesText = "Dice values: " + diceValues.join(", ");
-	var scoreText = "Score: " + score;
-	document.getElementById("dice-values").innerHTML = diceValuesText;
-	document.getElementById("score").innerHTML = scoreText;
+        // Select random dice image for each player
+        var randomIndex1 = Math.floor(Math.random() * diceImages.length);
+        var randomIndex2 = Math.floor(Math.random() * diceImages.length);
+        var diceImage1 = diceImages[randomIndex1];
+        var diceImage2 = diceImages[randomIndex2];
+
+        document.querySelector(".img1").setAttribute("src", diceImage1);
+        document.querySelector(".img2").setAttribute("src", diceImage2);
+
+        // Compare dice rolls and determine winner
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML = "Draw!";
+        } else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML = (player2 + " WINS!");
+        } else {
+            document.querySelector("h1").innerHTML = (player1 + " WINS!");
+        }
+    }, 2500);
 }
